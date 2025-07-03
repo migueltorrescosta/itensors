1a375b91adbde1c1c14e6ecd25fef89b602c0046
Miguel Costa <miguelptcosta1995@gmail.com>
2025-07-03 20:36:03 +0200

    Improve Index tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,13 +1,18 @@
+import numpy as np
+
 from itensors.struct import Index
 
 
-def test_index_has_correct_dimension():
+def test_indices_initializes_with_the_correct_dimension():
     for k in range(100):
         assert Index(k).dimension == k
 
 
-def test_index_has_unique_uuid_assigned():
-    for k in range(100):
-        i = Index(k)
-        j = Index(k)
-        assert i.id != j.id
+def test_indices_initialize_with_a_distinct_uuid():
+    number_of_samples = 10 ** 12
+    # The length of the set of index ids is smaller if any id clashes
+    assert len({
+        Index(np.random.randint(10)).id
+        for _
+        in range(number_of_samples)
+    }) == number_of_samples
