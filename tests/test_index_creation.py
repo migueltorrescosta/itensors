db80e726f9f1fbaf983da4b44f181daec34f80d3
Miguel Costa <miguelptcosta1995@gmail.com>
2025-07-04 12:26:09 +0200

    Remove irrelevant test & improve test naming

--- tests/test_index_creation.py
+++ tests/test_index_creation.py
@@ -3,12 +3,12 @@ import numpy as np
 from itensors.struct import Index
 
 
-def test_indices_initializes_with_the_correct_dimension():
+def test_index_creation_succeeds_with_the_correct_dimension():
     for k in range(100):
         assert Index(k).dimension == k
 
 
-def test_indices_initialize_with_a_distinct_uuid():
+def test_index_creation_with_distinct_uuids():
     number_of_samples = 10 ** 12
     # The length of the set of index ids is smaller if any id clashes
     assert len({
--- tests/test_itensor_creation.py
+++ tests/test_itensor_creation.py
@@ -10,14 +10,9 @@ def test_itensor_creation_succeeds():
     ITensor(np.random.rand(*shape), indices)
 
 
+s
 def test_itensor_creation_fails_on_shape_mismatch():
     shape = [3, 4]
     indices = [Index(k) for k in shape]
     with pytest.raises(Exception):
         ITensor(np.random.rand(*[4, 3]), indices)
-
-
-def test_addition_is_commutative():
-    x = 2
-    y = 3
-    assert x + y == y + x
